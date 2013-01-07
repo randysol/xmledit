ada048b062f98507b14e873c85262834f80eaa2a
Randy Solton <rsolton@apigee.com>
2013-01-06 16:10:16 -0800

    Quota class

--- js/test-xmledit.js
+++ js/test-xmledit.js
@@ -67,7 +67,67 @@
             });
             var xmlParser = new XMLParser();
             xmlParser.init(this.model.get("value"));
-            console.log("randuy", this.model.get("value"));
+            xmlParser.addSchemaDefinition({
+                className: "Quota",
+                elements: {
+                    "Allow" : {
+                        name: "Allow",
+                        attributes: {
+                            count : {
+                                type: "integer"
+                            },
+                            countRef: {
+                                type: "string"
+                            }
+                        }
+                    },
+                    "Interval": {
+                        name: "Interval",
+                        attributes: {
+                            ref: {
+                                type: "string"
+                            }
+                        },
+                        text: {
+                            type: "integer",
+                            restrictionsRange:  {
+                                min: 1,
+                                max: 100000
+                            }
+                        }
+                    },
+                    "Distributed": {
+                        name: "Distributed",
+                        text: {
+                            type: "boolean"
+                        }
+                    },
+                    "Synchronous": {
+                        name: "Synchronous",
+                        text: {
+                            type: "boolean"
+                        }
+                    },
+                    "TimeUnit": {
+                        name: "TimeUnit",
+                        attributes: {
+                             ref: {
+                                 type: "string"
+                             }
+                         },
+                        text: {
+                            type: "string",
+                            restrictionsEnum: [
+                                "second",
+                                "minute",
+                                "hour",
+                                "day",
+                                "month"
+                            ]
+                        }
+                    }
+                }
+            });
             this.model.set({ parsedRoot: xmlParser.parse()});
         }
     });
